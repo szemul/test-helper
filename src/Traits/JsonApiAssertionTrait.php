e7e67cd073c5c76c85964d2681e9f21cd0d6dc68
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2021-11-13 14:16:52 +1300

    Add assertUnprocessableExceptionParamErrors

--- src/Traits/JsonApiAssertionTrait.php
+++ src/Traits/JsonApiAssertionTrait.php
@@ -5,6 +5,7 @@ namespace Szemul\TestHelper\Traits;
 
 use PHPUnit\Framework\Assert;
 use Psr\Http\Message\ResponseInterface;
+use Szemul\SlimErrorHandlerBridge\Exception\HttpUnprocessableEntityException;
 
 trait JsonApiAssertionTrait
 {
@@ -17,5 +18,13 @@ trait JsonApiAssertionTrait
     ): void {
         Assert::assertSame($expectedStatusCode, $actual->getStatusCode(), $message);
         Assert::assertEquals($expectedBody, json_decode((string)$actual->getBody(), true), $message);
+    }
+
+    protected function assertUnprocessableExceptionParamErrors(
+        array $expected,
+        HttpUnprocessableEntityException $e,
+        string $message = '',
+    ): void {
+        $this->assertEquals($expected, json_decode(json_encode($e->getParameterErrors()), true), $message);
     }
 }
